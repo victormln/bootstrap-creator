7852141cd1bc7080efee935f41310ec79d8ab6f2
Victor <victormlndev@gmail.com>
2017-01-17 01:14:36 +0100

    Añadiendo todo

--- bootstrap-creator.sh
+++ bootstrap-creator.sh
@@ -21,12 +21,15 @@
 
 #  Descripción: Subida de los cambios a un repositorio git (ejecutar el script donde se tenga el .git)
 
-#####################
-# Colores de los mensajes
+# Mensajes de color
 ERROR='\033[0;31m'
 OK='\033[0;32m'
 WARNING='\033[1;33m'
 NC='\033[0m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
 
 # Cogemos los datos del archivo .conf
 source $( dirname "${BASH_SOURCE[0]}" )/user.conf
@@ -36,25 +39,94 @@ source $( dirname "${BASH_SOURCE[0]}" )/user.conf
 chmod +x $( dirname "${BASH_SOURCE[0]}" )/update.sh
 # Comprobaré si hay alguna versión nueva del programa autopush
 # y lo mostraré en pantalla
-source $( dirname "${BASH_SOURCE[0]}" )/update.sh
+#source $( dirname "${BASH_SOURCE[0]}" )/update.sh
 
-directorio_actual=$(pwd)
+function showHelp {
+  echo -e "usage: bootstrap-creator name_of_project [--title] [--cdn] - Script que te permite poner un bootstrap los archivos necesarios para tener un bootstrap en un solo comando."
+
+  echo -e "\n${CYAN}name_of_project${NC}"
+  echo -e "\tEs donde se guardará el bootstrap. Este parámetro es de uso obligatorio."
+  echo -e "\tTambién puedes añadir una ruta."
+
+  echo -e "\n${CYAN}[--title] nombre_del_title${NC}"
+  echo -e "\tSi pones este parámetro seguido de un nombre de título, te pondrá ese título entre los tags <title> del html"
+  echo -e "\tSi no pones este parámetro, el tag <title> estará vacío."
+
+  echo -e "\n${CYAN}[--cdn]${NC}"
+  echo -e "\tSi pones este parámetro, en vez de los archivos locales de css y js, tendrás un enlace a los archivos en CDN"
+  echo -e "\tSi no pones este parámetro, se te pondrán los archivos de bootstrap localmente."
+
+}
 
 # Hacer con array associativo
 function parseOption {
   if [ -z $1 ]
   then
-    copyBootstrap
+    echo -e "${ERROR}[ERROR] ${NC}Por favor, mínimo tiene que poner un parámetro."
+    echo "Puedes ver la ayuda del scrip con --help"
   else
-    if [ "$1" == "--title" ]
+    if [ "$1" == "--help" ]
     then
-
-    elif [ "$1" == "--cdn" ]
-    then
-
-    elif [ "$1" == "--" ]
-    then
-
+      showHelp
+    else
+      parseTitle $2 $3
+      mkdir -p $1
+      touch $1/index.html
+      cp bootstrap-files/template.html $1/index.html
+      fillTemplate $1
+      copyDate $1
     fi
   fi
 }
+
+function fillTemplate {
+  if $cdn
+  then
+    echo "1"
+    sed -n "/<\/title>/r bootstrap-files/header_cdn.html" $1/index.html
+  else
+    echo "12"
+    sed -n "/<\/title>/r bootstrap-files/header_local.html" $1/index.html
+  fi
+  #
+  echo "Titulo: $title"
+  if [ -z $title ]
+  then
+    sed -i 's/<titulo>//g'  $1/index.html
+  else
+    sed -i "s/<titulo>/$title/g" $1/index.html
+  fi
+}
+
+function copyDate {
+  cp -R bootstrap-files/$version_bootstrap/* $1
+}
+
+# Dados dos argumentos --title y "Nombre del <title>"
+function parseTitle {
+  if [ "$1" == "--title" ]
+  then
+    project_name=$(echo "$2" | grep "^--")
+    if [ -z $project_name ]
+    then
+      title=$2
+    else
+      echo -e "${ERROR}[ERROR] ${NC}Por favor, si pone el parámetro --title, le hará falta poner el título de la página."
+      echo "Si no quiere ningún title por defecto, no ponga el parámetro --title"
+      echo "Uso: --title \"Titulo del <title>\""
+      exit
+    fi
+  fi
+}
+
+function cdn {
+  if [ "$1" == "--cdn"]
+  then
+    cdn=true
+  fi
+}
+IFS=''
+directorio_actual=$(pwd)
+cdn=false
+title=""
+parseOption $1 $2 $3 $4
